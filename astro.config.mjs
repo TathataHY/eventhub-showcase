1b0a1d0f1195c305148cc5a6429b91d0170e5ac8
Andre Yovera <andre.yovera1@gmail.com>
2025-03-19 00:19:49 -0300

    Configuración inicial de EventHub Showcase

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://tathatahy.github.io',
+  base: '/EventHub-Showcase',
+  integrations: [tailwind()],
+  // Asegurar que tenemos la configuración correcta para GitHub Pages
+  outDir: './dist',
+  build: {
+    assets: 'assets'
+  }
+});
